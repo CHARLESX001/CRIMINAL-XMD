ec2b3be7990e4984fcab6090fb5372affcf92be8
CHARLESX001 <ajwangjunior7@gmail.com>
2025-08-15 14:19:59 +0300

    Update mention-reply.js

--- plugins/mention-reply.js
+++ plugins/mention-reply.js
@@ -26,7 +26,7 @@ cmd({
     const botNumber = conn.user.id.split(":")[0] + '@s.whatsapp.net';
 
     if (m.mentionedJid.includes(botNumber)) {
-      const thumbnailRes = await axios.get(config.MENU_IMAGE_URL || "https://files.catbox.moe/c836ws.png", {
+      const thumbnailRes = await axios.get(config.MENU_IMAGE_URL || "https://files.catbox.moe/o4of0n.jpg", {
         responseType: 'arraybuffer'
       });
       const thumbnailBuffer = Buffer.from(thumbnailRes.data, 'binary');
@@ -40,13 +40,13 @@ cmd({
           forwardingScore: 999,
           isForwarded: true,
           externalAdReply: {
-            title: config.BOT_NAME || "KHAN-MD 🥀",
-            body: config.DESCRIPTION || "POWERED BY JAWAD TECHX 🤌💗",
+            title: config.BOT_NAME || " 🥀",
+            body: config.DESCRIPTION || "POWERED BY CHARLES XMD 🤌💗",
             mediaType: 1,
             renderLargerThumbnail: false,
             thumbnail: thumbnailBuffer,
-            mediaUrl: "https://files.catbox.moe/l2t3e0.jpg", // Static image URL
-            sourceUrl: "https://wa.me/message/INB2QVGXHQREO1",
+            mediaUrl: "https://files.catbox.moe/o4of0n.jpg", // Static image URL
+            sourceUrl: "https://wa.me/message/+254759626063",
             showAdAttribution: true
           }
         }
