801fbc58463629b8009b64f7d78c6cc3ab14652a
CHARLESX001 <ajwangjunior7@gmail.com>
2025-08-17 16:23:37 +0300

    Update prayertime.js

--- plugins/prayertime.js
+++ plugins/prayertime.js
@@ -60,8 +60,8 @@ async(conn, mek, m, {from, l, quoted, body, isCmd, command, args, q, isGroup, se
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363288304618280@newsletter',
-                        newsletterName: 'pkxmd',
+                        newsletterJid: '120363351653122969@newsletter',
+                        newsletterName: 'CRIMINAL XMD',
                         serverMessageId: 143
                     }
                 }
